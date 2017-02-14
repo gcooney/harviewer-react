7633e3d042e72074d5d0bd1aec5c762c414543e6
gitgrimbo <gitgrimbo@gmail.com>
2017-02-14 09:51:15 +0000

    Add .nojekyll file during deployment
    So that /node_modules resources (e.g. jquery.js and URI.js) will be served.
    
    https://github.com/blog/572-bypassing-jekyll-on-github-pages

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -9,8 +9,8 @@ const repo = require("../package.json").repository.url;
 set("+v");
 
 if (!which("git")) {
-    echo("git is required");
-    exit(1);
+  echo("git is required");
+  exit(1);
 }
 
 
@@ -20,16 +20,16 @@ rm("-rf", ghTemp);
 mkdir("-p", ghTemp);
 
 if (exec("git clone " + repo + " --branch gh-pages --single-branch " + ghTemp).code !== 0) {
-    echo("git clone failed");
-    exit(1);
+  echo("git clone failed");
+  exit(1);
 }
 
 
 // Run the build and copy the built resources to staging
 
 if (exec("npm run webapp-build-prod").code !== 0) {
-    echo("webpack build failed");
-    exit(1);
+  echo("webpack build failed");
+  exit(1);
 }
 
 cp("-r", "./dist/*", ghTemp);
@@ -43,9 +43,9 @@ cp("-r", "./webapp/*", ghTemp);
 // Copy other required node_modules to staging
 
 function copyAndPreserveFolder(dest, folder, file) {
-    const src = path.join(folder, file);
-    mkdir("-p", path.join(dest, folder));
-    cp(src, path.join(dest, folder));
+  const src = path.join(folder, file);
+  mkdir("-p", path.join(dest, folder));
+  cp(src, path.join(dest, folder));
 }
 
 copyAndPreserveFolder(ghTemp, "node_modules/jquery/dist", "jquery.js");
@@ -55,16 +55,18 @@ copyAndPreserveFolder(ghTemp, "node_modules/urijs/src", "URI.js");
 // Fix the path differences between dev and prod.
 
 function fixLinks(file) {
-    // When deploying we don't want to move 'out' of the webapp to locate the
-    // node_modules folder.
-    let str = fs.readFileSync(file, { encoding: "utf-8" });
-    str = str.replace("../node_modules/", "node_modules/");
-    fs.writeFileSync(file, str);
+  // When deploying we don't want to move 'out' of the webapp to locate the
+  // node_modules folder.
+  let str = fs.readFileSync(file, { encoding: "utf-8" });
+  str = str.replace("../node_modules/", "node_modules/");
+  fs.writeFileSync(file, str);
 }
 
 fixLinks(path.join(ghTemp, "index.html"));
 fixLinks(path.join(ghTemp, "demos", "demo-shell.html"));
 
+// https://github.com/blog/572-bypassing-jekyll-on-github-pages
+touch(path.join(ghTemp, ".nojekyll"));
 
 // Add the staging files to the gh-pages branch
 
