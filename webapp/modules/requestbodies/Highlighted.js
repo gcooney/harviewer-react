d93824a9a2b8720bbc8c3e62ff89cd96db1b66f0
gitgrimbo <gitgrimbo@gmail.com>
2018-06-13 20:53:35 +0100

    Syntax highlighting with Prism

--- webapp/modules/requestbodies/Highlighted.js
+++ webapp/modules/requestbodies/Highlighted.js
@@ -1,48 +1,66 @@
 import React from "react";
 import PropTypes from "prop-types";
-import createReactClass from "create-react-class";
 import * as Mime from "../core/mime";
 
 import { canDecode } from "./decoder";
 
-const Highlighted = createReactClass({
-  displayName: "requestbodies/Highlighted",
-
-  propTypes: {
-    entry: PropTypes.object,
-  },
+class Highlighted extends React.Component {
+  constructor(props) {
+    super(props);
+    this.domRef = React.createRef();
+  }
 
   maybeDoHighlighting() {
     const { entry } = this.props;
-    const pre = this.refs.dom;
-    pre.innerText = "";
     const text = entry.response.content.text;
-    pre.appendChild(document.createTextNode(text));
-    SyntaxHighlighter.highlight(pre);
-  },
+
+    const pre = this.domRef.current;
+    const code = pre.firstChild;
+
+    // clear flag
+    code.removeAttribute("highlighted");
+
+    code.innerText = "";
+    code.appendChild(document.createTextNode(text));
+    // Run highlightElement on the <code>, not the <pre>
+    Prism.highlightElement(code);
+
+    // set flag that is useful for testing - to determine if highlighting has worked.
+    const brush = Highlighted.shouldHighlightAs(entry.response.content.mimeType);
+    if (code.classList.contains(`language-${brush}`)) {
+      code.setAttribute("highlighted", true);
+    }
+  }
 
   componentDidMount() {
     this.maybeDoHighlighting();
-  },
+  }
 
   componentDidUpdate() {
     this.maybeDoHighlighting();
-  },
+  }
 
   render() {
     const { entry } = this.props;
     const brush = Highlighted.shouldHighlightAs(entry.response.content.mimeType);
+    // line-numbers class must come after language.
+    // Prism needs a <code> block inside a <pre> block.
     return (
       <div className="netInfoHighlightedText netInfoText">
-        <pre className={"toolbar: false; brush: " + brush} name="code" ref="dom">
+        <pre className={`language-${brush} line-numbers`} name="code" ref={this.domRef}>
+          <code></code>
         </pre>
       </div>
     );
   }
-});
+};
+
+Highlighted.propTypes = {
+  entry: PropTypes.object,
+};
 
 Highlighted.canShowEntry = function(entry) {
-  var content = entry.response.content;
+  const { content } = entry.response;
 
   if (!content || !content.text) {
     return false;
@@ -53,30 +71,42 @@ Highlighted.canShowEntry = function(entry) {
   }
 
   // Remove any mime type parameters (if any)
-  var mimeType = Mime.extractMimeType(content.mimeType || "");
+  const mimeType = Mime.extractMimeType(content.mimeType || "");
 
-  return (Highlighted.shouldHighlightAs(mimeType) !== null);
+  const shouldHighlightAs = Highlighted.shouldHighlightAs(mimeType);
+  return (shouldHighlightAs !== null);
 };
 
-// TODO - REPLACE ME
-const XmlTab = {
-  isXmlMimeType: mimeType => false
+// TODO - Put this in the right place when we implement XML tab.
+const XmlTab = {};
+
+XmlTab.isXmlMimeType = function(mimeType) {
+  mimeType = Mime.extractMimeType(mimeType);
+  return [
+    "text/xml",
+    "application/xml",
+    "image/svg+xml",
+    "application/atom+xml",
+    "application/xslt+xml",
+    "application/mathml+xml",
+    "application/rss+xml",
+  ].indexOf(mimeType) > -1;
 };
 
 Highlighted.shouldHighlightAs = function(mimeType) {
-  var mimeTypesToHighlight = {
+  const mimeTypesToHighlight = {
     javascript: [
       "application/javascript",
       "text/javascript",
       "application/x-javascript",
       "text/ecmascript",
       "application/ecmascript",
-      "application/json"
+      "application/json",
     ],
     css: ["text/css"],
-    html: ["text/html", "application/xhtml+xml"]
+    markup: ["text/html", "application/xhtml+xml"],
   };
-  for (var brush in mimeTypesToHighlight) {
+  for (let brush in mimeTypesToHighlight) {
     if (mimeTypesToHighlight[brush].indexOf(mimeType) > -1) {
       return brush;
     }
