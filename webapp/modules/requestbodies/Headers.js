681ca7da1ad146bbf7045b437817a91eb364dea6
gitgrimbo <gitgrimbo@gmail.com>
2018-06-13 20:58:19 +0100

    Extract ParamRow and TitleRow from Headers
    As ParamRow will be used in SendData and URLParameters request body
    tabs.

--- webapp/modules/requestbodies/Headers.js
+++ webapp/modules/requestbodies/Headers.js
@@ -1,50 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import createReactClass from "create-react-class";
-import * as Str from "../core/string";
 
-function getParamValue(value) {
-  // This value is inserted into PRE element and so, make sure the HTML isn't escaped (1210).
-  // This is why the second parameter is true.
-  // The PRE element preserves whitespaces so they are displayed the same, as they come from
-  // the server (1194).
-  return Str.wrapText(value, true);
-}
+import ParamRow from "./ParamRow";
+import TitleRow from "./TitleRow";
 
-const ParamRow = props => {
-  return (
-    <tr>
-      <td className="netInfoParamName">{props.name}</td>
-      <td className="netInfoParamValue"><pre>{getParamValue(props.value)}</pre></td>
-    </tr>
-  );
-};
-
-ParamRow.propTypes = {
-  name: PropTypes.string,
-  value: PropTypes.string,
-};
-
-const TitleRow = props => {
-  return (
-    <tr className={"netInfo" + props.titleType + "HeadersTitle"}>
-      <td colSpan="2"><div className="netInfoHeadersGroup ">{props.title}</div></td>
-    </tr>
-  );
-};
-
-TitleRow.propTypes = {
-  title: PropTypes.string,
-  titleType: PropTypes.string,
-};
-
-export default createReactClass({
-  displayName: "requestbodies/Headers",
-
-  propTypes: {
-    entry: PropTypes.object,
-  },
-
+class Headers extends React.Component {
   render() {
     const { entry } = this.props;
 
@@ -67,4 +27,10 @@ export default createReactClass({
       </table>
     );
   }
-});
+}
+
+Headers.propTypes = {
+  entry: PropTypes.object,
+};
+
+export default Headers;
