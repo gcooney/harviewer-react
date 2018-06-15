d4852a6eddfeb04fdbe8cf0e0d949e0d86c78178
gitgrimbo <gitgrimbo@gmail.com>
2018-06-15 14:09:16 +0100

    Add fake JSON contents

--- webapp/modules/requestbodies/JSON.js
+++ webapp/modules/requestbodies/JSON.js
@@ -13,7 +13,50 @@ class JSON extends React.Component {
     const { entry } = this.props;
 
     return (
-      <div>JSON</div>
+      <table cellPadding="0" cellSpacing="0" className="domTable">
+        <tbody className="">
+          <tr level="0" className="memberRow domRow hasChildren hasChildren opened">
+            <td style={{ paddingLeft: "0px" }} className="memberLabelCell">
+              <span className="memberLabel domLabel">log</span>
+            </td>
+            <td className="memberValueCell">
+              <div className="objectBox objectBox-object">TODO - THIS IS FAKE</div>
+            </td>
+          </tr>
+          <tr level="1" className="memberRow domRow">
+            <td style={{ paddingLeft: "16px" }} className="memberLabelCell">
+              <span className="memberLabel domLabel">version</span>
+            </td>
+            <td className="memberValueCell">
+              <div className="objectBox objectBox-string">1.2</div>
+            </td>
+          </tr>
+          <tr level="1" className="memberRow domRow hasChildren hasChildren">
+            <td style={{ paddingLeft: "16px" }} className="memberLabelCell">
+              <span className="memberLabel domLabel">creator</span>
+            </td>
+            <td className="memberValueCell">
+              <div className="objectBox objectBox-object">Object</div>
+            </td>
+          </tr>
+          <tr level="1" className="memberRow domRow hasChildren hasChildren">
+            <td style={{ paddingLeft: "16px" }} className="memberLabelCell">
+              <span className="memberLabel domLabel">pages</span>
+            </td>
+            <td className="memberValueCell">
+              <div className="objectBox objectBox-array">Array [1]</div>
+            </td>
+          </tr>
+          <tr level="1" className="memberRow domRow hasChildren hasChildren">
+            <td style={{ paddingLeft: "16px" }} className="memberLabelCell">
+              <span className="memberLabel domLabel">entries</span>
+            </td>
+            <td className="memberValueCell">
+              <div className="objectBox objectBox-array">Array [1]</div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
     );
   }
 };
