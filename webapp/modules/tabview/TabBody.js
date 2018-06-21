ef5b9384b603b388d2f32cfe356924ead141dad0
gitgrimbo <gitgrimbo@gmail.com>
2018-06-21 11:19:24 +0100

    class-ify TabBody

--- webapp/modules/tabview/TabBody.js
+++ webapp/modules/tabview/TabBody.js
@@ -1,22 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
-import createReactClass from "create-react-class";
 
-export default createReactClass({
-  displayName: "tabview/TabBody",
-
-  propTypes: {
-    children: PropTypes.node,
-    id: PropTypes.string,
-    selected: PropTypes.bool,
-  },
-
+class TabBody extends React.Component {
   render() {
     const { id, selected } = this.props;
+    const selectedClass = selected ? "selected" : "";
     return (
-      <div className={"tab" + id + "Body tabBody " + (selected ? "selected" : "")}>
+      <div className={`tab${id}Body tabBody ${selectedClass}`}>
         {this.props.children}
       </div>
     );
-  },
-});
+  }
+}
+
+TabBody.propTypes = {
+  children: PropTypes.node,
+  id: PropTypes.string,
+  selected: PropTypes.bool,
+};
+
+export default TabBody;
