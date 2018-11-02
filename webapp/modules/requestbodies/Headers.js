dd5a62b99512f06f2d8c7f8dc567d911431becce
gitgrimbo <gitgrimbo@gmail.com>
2018-11-02 16:24:28 +0000

    add request URL to HeadersTab

--- webapp/modules/requestbodies/Headers.js
+++ webapp/modules/requestbodies/Headers.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import ParamRow from "./ParamRow";
 import TitleRow from "./TitleRow";
 
+import * as Str from "../core/string";
+
 class Headers extends React.Component {
   render() {
     const { entry } = this.props;
@@ -12,26 +14,29 @@ class Headers extends React.Component {
     const requestHeaders = entry.request.headers || [];
 
     return (
-      <table cellPadding="0" cellSpacing="0" className="netInfoHeadersText netInfoText netInfoHeadersTable ">
-        <tbody>
-          <TitleRow titleType="ResponseVersion" title="Response Version" />
-          <ParamRow name="" value={entry.response.httpVersion} />
-          <TitleRow titleType="ResponseHeaders" title="Response Headers" />
-          {
-            responseHeaders.map((header, i) =>
-              <ParamRow key={i} name={header.name} value={header.value} />
-            )
-          }
-          <TitleRow titleType="RequestVersion" title="Request Version" />
-          <ParamRow name="" value={entry.request.httpVersion} />
-          <TitleRow titleType="RequestHeaders" title="Request Headers" />
-          {
-            requestHeaders.map((header, i) =>
-              <ParamRow key={i} name={header.name} value={header.value} />
-            )
-          }
-        </tbody>
-      </table>
+      <div>
+        <a className="netInfoRequestURL" href={entry.request.url}>{Str.cropString(entry.request.url, 128)}</a>
+        <table cellPadding="0" cellSpacing="0" className="netInfoHeadersText netInfoText netInfoHeadersTable ">
+          <tbody>
+            <TitleRow titleType="ResponseVersion" title="Response Version" />
+            <ParamRow name="" value={entry.response.httpVersion} />
+            <TitleRow titleType="ResponseHeaders" title="Response Headers" />
+            {
+              responseHeaders.map((header, i) =>
+                <ParamRow key={i} name={header.name} value={header.value} />
+              )
+            }
+            <TitleRow titleType="RequestVersion" title="Request Version" />
+            <ParamRow name="" value={entry.request.httpVersion} />
+            <TitleRow titleType="RequestHeaders" title="Request Headers" />
+            {
+              requestHeaders.map((header, i) =>
+                <ParamRow key={i} name={header.name} value={header.value} />
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
