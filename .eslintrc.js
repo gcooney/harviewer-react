738f938418759987f084c4118f6af4e6cc2d0f8d
gitgrimbo <gitgrimbo@gmail.com>
2018-11-02 09:26:46 +0000

    lint: react 16.6.0 and exclude legacy code for the moment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: [
+  "parser": "babel-eslint",
+  "extends": [
     "google",
     "plugin:react/recommended",
   ],
-  plugins: [
+  "settings": {
+    "react": {
+      "version": "16.6.0",
+    },
+  },
+  "plugins": [
     "babel",
     "react",
   ],
-  rules: {
+  "rules": {
     "indent": ["error", 2],
     "linebreak-style": "off",
 
     "max-len": ["warn", {
-      code: 100,
-      tabWidth: 2,
-      ignoreUrls: true,
+      "code": 100,
+      "tabWidth": 2,
+      "ignoreUrls": true,
       // JSX expression: Line starting with spaces then "<"
       // JSX assignment: Line containing " = <\w"
       // JSX return: Line starting with "return <\w"
-      ignorePattern: "^ *<| = <\\w|^ *return <\\w",
+      "ignorePattern": "^ *<| = <\\w|^ *return <\\w",
     }],
 
     // E.g. to allow
@@ -36,11 +41,11 @@ module.exports = {
     "no-invalid-this": "off",
     "babel/no-invalid-this": "error",
   },
-  env: {
-    browser: true,
-    jquery: true,
+  "env": {
+    "browser": true,
+    "jquery": true,
   },
-  parserOptions: {
+  "parserOptions": {
     "ecmaVersion": 6,
     "sourceType": "module",
     "ecmaFeatures": {
