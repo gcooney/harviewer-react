f3e175bef67c88ca99bcb644f37368c6b1bfa33e
gitgrimbo <gitgrimbo@gmail.com>
2018-12-13 10:34:01 +0000

    wip: DOMTab search prints search results to console

--- webapp/modules/tabs/dom/DOMTab.js
+++ webapp/modules/tabs/dom/DOMTab.js
@@ -3,14 +3,44 @@ import PropTypes from "prop-types";
 
 import DOMBox from "./DOMBox";
 import Toolbar from "../../toolbar/Toolbar";
+import ObjectSearch from "../../tabs/ObjectSearch";
+
+const caseSensitiveCookieName = "searchCaseSensitive";
 
 class SearchBox extends React.Component {
+  state = {
+    searchValue: "",
+    status: null,
+  }
+
+  onChange = (e) => {
+    const text = e.target.value;
+    this.setState({
+      searchValue: text,
+    }, () => this.search(text));
+  }
+
+  onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      this.search(e.target.value);
+    }
+  }
+
+  search(text) {
+    const result = this.props.search(text);
+    this.setState({
+      status: result ? null : "notfound",
+    });
+  }
+
   render() {
+    const { status } = this.state;
     return (
       <span className="searchBox">
         <span className="toolbarSeparator resizer">&nbsp;</span>
         <span className="searchTextBox">
-          <input type="text" placeholder="Search" className="searchInput" />
+          <input type="text" placeholder="Search" className="searchInput" status={status} value={this.state.searchValue} onChange={this.onChange} onKeyPress={this.onKeyPress} />
           <span className="arrow">&nbsp;</span>
         </span>
       </span>
@@ -19,7 +49,30 @@ class SearchBox extends React.Component {
 }
 
 class DOMTab extends Component {
-  onClick = (e) => { }
+  search = (text) => {
+    if (text.length < 3) {
+      return true;
+    }
+
+    if (this.currentSearch && this.currentSearch.text !== text) {
+      console.log("new text, clear search");
+      this.currentSearch = null;
+    }
+
+    if (!this.currentSearch) {
+      console.log("create search");
+      const { harModels } = this.props;
+      const inputs = harModels;
+      this.currentSearch = new ObjectSearch(text, inputs, false, caseSensitiveCookieName);
+    }
+
+    if (this.currentSearch.findNext(text)) {
+      console.log(this.currentSearch);
+      return true;
+    }
+
+    return false;
+  }
 
   getTitle(har) {
     // Iterate all pages and get titles.
@@ -33,7 +86,7 @@ class DOMTab extends Component {
     return (
       <div key="Toolbar" className="domToolbar">
         <Toolbar>
-          <SearchBox />
+          <SearchBox search={this.search} />
         </Toolbar>
       </div>
     );
